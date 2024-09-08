6ecf786f64ddcd9985f336e18f9074a33bc03a11
Jonathan Moran <jmoran@uliege.be>
2024-09-08 17:48:03 +0200

    parallel determinant/eigen computation

--- active_learning/active_learning.py
+++ active_learning/active_learning.py
@@ -1,15 +1,31 @@
 import torch
 import numpy as np
+from joblib import Parallel, delayed
 
 class BatchActiveLearning():
     def __init__(self, n_active_samples):
         print('engine humming...')
         self.n_active_samples = n_active_samples
+
+    def calculate_determinant(self, x_mc, model, sample, selected_indices):
+        x_assemble = x_mc[selected_indices + [sample]]
+        _, cov_assemble = model.predict(x_assemble, return_std=False, return_cov=True)
+        return np.linalg.det(cov_assemble)
     
-    def get_random(self, idx_pool):
-        # create a list of random numbers as integers from 0 to len(idx_pool)
-        n_active_points = self.num_active_points
-        random_idx_pool = np.random.choice(idx_pool, n_active_points, replace=False)
+    def calculate_eigen(self, x_mc, model, sample, selected_indices):
+        x_assemble = x_mc[selected_indices + [sample]]
+        _, cov_assemble = model.predict(x_assemble, return_std=False, return_cov=True)
+        eigen_values, _ = np.linalg.eig(cov_assemble)
+        return np.sum(eigen_values)
+    
+    def get_random(self, x_mc, samples=None):
+        print('random')
+        if samples is None:
+            act_samples = self.n_active_samples
+        else:
+            act_samples = samples
+
+        random_idx_pool = np.random.choice(np.arange(len(x_mc)), act_samples, replace=False)
         random_idx_pool = random_idx_pool.tolist()
         return random_idx_pool
 
@@ -46,6 +62,63 @@ class BatchActiveLearning():
 
             #evaluate U_function normalised with det_cov
             u_function = (mean_prediction.abs())/det_cov
+
+            #avoid selected values
+            u_function[selected_indices] = np.inf
+
+            #adding selected indices
+            _, u_min_idx = u_function.topk(1, largest=False)
+            selected_indices.append(u_min_idx.item())
+
+        return selected_indices
+    
+    def get_correlation_det(self, x_mc, model, mean_prediction, std_prediction, samples=None):
+        print('determinant')
+        if samples is None:
+            act_samples = self.n_active_samples
+        else:
+            act_samples = samples
+        #firs sample evaluated with U_function
+        selected_indices = self.get_u_function(mean_prediction, std_prediction, samples=1)
+
+        for _ in range(act_samples - 1):
+            # Covariance computation
+            # Use parallel processing to calculate the determinants for all samples
+            det_cov = Parallel(n_jobs=-1)(delayed(self.calculate_determinant)(x_mc, model, sample, selected_indices) for sample in range(len(x_mc)))
+        
+            det_cov = torch.tensor(det_cov)
+
+            #evaluate U_function normalised with det_cov
+            u_function = (mean_prediction.abs())/det_cov
+
+            #avoid selected values
+            u_function[selected_indices] = np.inf
+
+            #adding selected indices
+            _, u_min_idx = u_function.topk(1, largest=False)
+            selected_indices.append(u_min_idx.item())
+
+        return selected_indices
+    
+    def get_correlation_eigen(self, x_mc, model, mean_prediction, std_prediction, samples=None):
+        print('eigen')
+        
+        if samples is None:
+            act_samples = self.n_active_samples
+        else:
+            act_samples = samples
+        #firs sample evaluated with U_function
+        selected_indices = self.get_u_function(mean_prediction, std_prediction, samples=1)
+
+        for _ in range(act_samples - 1):
+            # Covariance computation
+            # Use parallel processing to calculate the determinants for all samples
+            sum_eigen = Parallel(n_jobs=-1)(delayed(self.calculate_eigen)(x_mc, model, sample, selected_indices) for sample in range(len(x_mc)))
+        
+            sum_eigen = torch.tensor(sum_eigen)
+
+            #evaluate U_function normalised with det_cov
+            u_function = (mean_prediction.abs())/sum_eigen
 
             #avoid selected values
             u_function[selected_indices] = np.inf
--- main_batchactive.py
+++ main_batchactive.py
@@ -1,13 +1,14 @@
 import torch
 import numpy as np
-# import yaml
 from datetime import datetime
 import pickle
 import os
-# import wandb
 import argparse
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import Matern
+
+# import wandb
+import time
 
 from limit_states import REGISTRY as ls_REGISTRY
 from active_learning.active_learning import BatchActiveLearning
@@ -21,13 +22,16 @@ parser.add_argument('--ls', type=str, nargs='?', action='store', default='four_b
                     help='Specify target LS: four_branch, himmelblau, pushover_frame. Def: four_branch')
 
 parser.add_argument('--acq_f', type=str, nargs='?', action='store', default='u_function',
-                    help='Specify the acquisition function: correlation, u_function. Def: correlation')
+                    help='Specify the acquisition function: correlation_det, correlation_eigen, u_function, random. Def: u_function')
 
 parser.add_argument('--acq_b', type=int, nargs='?', action='store', default=3,
                     help='Specify the batch sample size per iteration. Def: 3.')
 
 parser.add_argument('--seed', type=int, nargs='?', action='store', default=None,
                     help='Specify the random seed. Def: Random')
+
+parser.add_argument('--n_exp', type=int, nargs='?', action='store', default=1,
+                    help='Specify the number of experiments to run. Def: 1')
 
 args = parser.parse_args()
 
@@ -37,88 +41,112 @@ lstate = ls_REGISTRY[args.ls]()
 
 #-----------------------------------------------------------------------------------------------
 # Loading experiment setting 
-doe =  10     # initial DoE with LHS
-budget = 100  # max number of samples
-n_mcs_pool = 1e5  # n_MonteCarlo pool of samples for learning
-n_mcs_pf = 1e6  # n_MonteCarlo pool of samples for pf estimation
+config={}
+doe = config['doe'] = 10     # initial DoE with LHS
+budget = config['budget'] = 100  # max number of samples
+n_mcs_pool = config['n_mcs_pool'] = 1e5  # n_MonteCarlo pool of samples for learning
+n_mcs_pf = config['n_mcs_pf']  = 1e6  # n_MonteCarlo pool of samples for pf estimation
 iterations = int((budget-doe)/args.acq_b) + 1 #iteration to complete the available budget-doe
-
-# # Create list of indices for learning pool
-# indices = torch.arange(int(n_mcs_pool))
 
 #results directory
 date_time_stamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 results_dir = f'results/{args.ls}/{args.acq_f}/{args.acq_b}/'
-results_file = {}
-pf_evol = []
 
 if not os.path.exists(results_dir):
     os.makedirs(results_dir)
 
-#experiment seed for reproducibility
-if args.seed is not None:
-    seed_exp = args.seed
-else:
-    seed_exp = np.random.randint(0, 2**30 - 1)
+print(f'Target LS: {args.ls}')
 
-np.random.seed(seed_exp)
-torch.manual_seed(seed_exp)
-random_state = np.random.RandomState(seed_exp)
+for exp in range(args.n_exp):
+    #results
+    results_file = {}
+    pf_evol = []
 
-print(f'Target LS: {args.ls}, experiment seed: {seed_exp}')
+    #experiment seed for reproducibility
+    if args.seed is not None:
+        seed_exp = args.seed
+    else:
+        seed_exp = np.random.randint(0, 2**30 - 1)
 
-#-----------------------------------------------------------------------------------------------
-# Design of experiments
-x_train_norm, _ , y_train = lstate.get_doe(n_samples=doe, method='lhs', random_state=random_state)
-x_train_norm = torch.tensor(x_train_norm)
-y_train = torch.tensor(y_train)
+    np.random.seed(seed_exp)
+    torch.manual_seed(seed_exp)
+    random_state = np.random.RandomState(seed_exp)
 
-# Loading active learning methods
-active_learning = BatchActiveLearning(n_active_samples= args.acq_b)
+    print(f'Experiment: {exp+1}/{args.n_exp}, Seed: {seed_exp}')
+    config['seed'] = seed_exp
+    results_file['config'] = config  
+    #-----------------------------------------------------------------------------------------------
+    # Design of experiments
+    x_train_norm, _ , y_train = lstate.get_doe(n_samples=doe, method='lhs', random_state=random_state)
+    x_train_norm = torch.tensor(x_train_norm)
+    y_train = torch.tensor(y_train)
 
-for it in range(iterations):
-# while len(x_train_norm) < budget+args.acq_b:
+    # Loading active learning methods
+    active_learning = BatchActiveLearning(n_active_samples= args.acq_b)
 
-    # Find the minimum and maximum distance between training samples
-    # min_distance, max_distance = min_max_distance(x_train_norm, x_train_norm)
-    # kernel = 1.0 * Matern(length_scale=1.0, length_scale_bounds=(min_distance, max_distance), nu=1.5)
-    
-    print(f'Training size: {len(x_train_norm)} ')
-    # Train the Gaussian Process model
-    kernel = 1.0 * Matern(length_scale=1.0, nu=1.5)
-    model_gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=9)
-    model_gp.fit(x_train_norm, y_train)
+    start_time = time.time()
 
-    # Pf estimation with MCs
-    # print(f'Pf estimation over MC pool...', end=" ")
-    x_mcs_pf = np.random.uniform(0, 1, size=(int(n_mcs_pf), lstate.input_dim))
-    mean_pf = model_gp.predict(x_mcs_pf, return_std=False)
-    Pf_model = np.sum(mean_pf < 0) / len(mean_pf)
-    print(f'Pf ref: {lstate.target:.3E}, Pf model: {Pf_model.item():.3E} \n')
-    # pf_evol.append(Pf_model)
-
-    # Making predictions of mean and std for mc population 
-    x_mc_pool = np.random.uniform(0, 1, size=(int(n_mcs_pool), lstate.input_dim))
-    mean_prediction, std_prediction = model_gp.predict(x_mc_pool, return_std=True)
-    mean_pred = torch.tensor(mean_prediction)
-    std_pred = torch.tensor(std_prediction)
+    for it in range(iterations):
+        # Find the minimum and maximum distance between training samples
+        # min_distance, _ = min_max_distance(x_train_norm, x_train_norm)
+        # kernel = 1.0 * Matern(length_scale=1.0, length_scale_bounds=(min_distance, 1000000), nu=1.5)
         
-    if args.acq_f == 'u_function':
-        selected_indices = active_learning.get_u_function(mean_pred, std_pred)
+        print(f'Training size: {len(x_train_norm)} samples', end=" ")
+        # Train the Gaussian Process model
+        kernel = 1.0 * Matern(length_scale=1.0, nu=1.5)
+        model_gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=9)
+        model_gp.fit(x_train_norm, y_train)
 
-    elif args.acq_f == 'correlation':
-        selected_indices = active_learning.get_correlation(x_mc_pool, model_gp, mean_pred, std_pred)
+        # Pf estimation with MCs
+        x_mcs_pf = np.random.uniform(0, 1, size=(int(n_mcs_pf), lstate.input_dim))
+        mean_pf = model_gp.predict(x_mcs_pf, return_std=False)
+        Pf_model = np.sum(mean_pf < 0) / len(mean_pf)
+        print(f'Pf ref: {lstate.target:.3E}, Pf model: {Pf_model.item():.3E} \n')
+        pf_evol.append(Pf_model)
 
-    # Get training and target samples
-    selected_samples_norm = x_mc_pool[selected_indices]
+        # Making predictions of mean and std for mc population 
+        x_mc_pool = np.random.uniform(0, 1, size=(int(n_mcs_pool), lstate.input_dim))
+        mean_prediction, std_prediction = model_gp.predict(x_mc_pool, return_std=True)
+        mean_pred = torch.tensor(mean_prediction)
+        std_pred = torch.tensor(std_prediction)
+            
+        if args.acq_f == 'u_function':
+            selected_indices = active_learning.get_u_function(mean_pred, std_pred)
 
-    selected_samples = isoprob_transform(selected_samples_norm, lstate.marginals)
-    selected_outputs = lstate.eval_lstate(selected_samples)
+        elif args.acq_f == 'correlation_det':
+            selected_indices = active_learning.get_correlation_det(x_mc_pool, model_gp, mean_pred, std_pred)
 
-    # Update the training set
-    x_train_norm = torch.cat((x_train_norm, torch.tensor(selected_samples_norm)), 0)
-    y_train = torch.cat((y_train, torch.tensor(selected_outputs)))
+        elif args.acq_f == 'correlation_eigen':
+            selected_indices = active_learning.get_correlation_eigen(x_mc_pool, model_gp, mean_pred, std_pred)
+        
+        elif args.acq_f == 'random':
+            selected_indices = active_learning.get_random(x_mc_pool)
 
-    # print(x_train_norm)
+        # Get training and target samples
+        selected_samples_norm = x_mc_pool[selected_indices]
 
-print("Active learning completed")
+        selected_samples = isoprob_transform(selected_samples_norm, lstate.marginals)
+        selected_outputs = lstate.eval_lstate(selected_samples)
+
+        # Update the training set
+        x_train_norm = torch.cat((x_train_norm, torch.tensor(selected_samples_norm)), 0)
+        y_train = torch.cat((y_train, torch.tensor(selected_outputs)))
+
+        #saving partial results
+        results_file['model'] = model_gp  
+        results_file['Pf_model'] = pf_evol
+
+        with open(results_dir+'exp_'+str(exp)+'_'+date_time_stamp+'.pkl', 'wb') as file_id:
+                    pickle.dump(results_file, file_id)
+
+    #saving final results
+    results_file['model'] = model_gp  
+    results_file['Pf_model'] = pf_evol
+    results_file['training_samples'] = x_train_norm, y_train  #training samples
+
+    with open(results_dir+'exp_'+str(exp)+'_'+date_time_stamp+'.pkl', 'wb') as file_id:
+                    pickle.dump(results_file, file_id)
+
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"Active learning completed in: {(execution_time/60):.2f} mins")
