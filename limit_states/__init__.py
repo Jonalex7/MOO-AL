58b229b066737294fb608ad3acffe5d8669e53ec
Jonathan <jmoran@uliege.be>
2024-10-14 14:29:46 +0200

    adding damped oscillator

--- limit_states/__init__.py
+++ limit_states/__init__.py
@@ -1,9 +1,11 @@
 from .g2d_four_branch import g2D_four_branch
 from .g2d_hmcmc_himmelblau import g2d_himmelblau
+from .g8d_dampedoscillator import g8d_damped_oscillator
 # from .g_fem_pushover import g_pushover
 
 REGISTRY = {}
 
 REGISTRY["four_branch"] = g2D_four_branch
 REGISTRY["himmelblau"] = g2d_himmelblau
+REGISTRY["damped_oscillator"] = g8d_damped_oscillator
 # REGISTRY["pushover_frame"] = g_pushover
