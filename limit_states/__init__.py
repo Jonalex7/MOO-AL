f260af52e47e133144b0f1fa78144cfa1df9c245
Jonathan <jmoran@uliege.be>
2024-10-16 11:38:48 +0200

    adding lstate rp55

--- limit_states/__init__.py
+++ limit_states/__init__.py
@@ -2,6 +2,7 @@ from .g2d_four_branch import g2D_four_branch
 from .g2d_hmcmc_himmelblau import g2d_himmelblau
 from .g8d_dampedoscillator import g8d_damped_oscillator
 from .g2d_hat import g2D_hat_function
+from .g2d_rp55 import g2D_rp55
 # from .g_fem_pushover import g_pushover
 
 REGISTRY = {}
@@ -10,4 +11,5 @@ REGISTRY["four_branch"] = g2D_four_branch
 REGISTRY["himmelblau"] = g2d_himmelblau
 REGISTRY["damped_oscillator"] = g8d_damped_oscillator
 REGISTRY["hat"] = g2D_hat_function
+REGISTRY["rp55"] = g2D_rp55
 # REGISTRY["pushover_frame"] = g_pushover
