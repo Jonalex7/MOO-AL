da22b6f0cf405cba064f8428b9d1548eba081ffd
Jonathan <jmoran@uliege.be>
2024-10-17 12:31:10 +0200

    adding le_frame LS

--- limit_states/__init__.py
+++ limit_states/__init__.py
@@ -3,6 +3,7 @@ from .g2d_hmcmc_himmelblau import g2d_himmelblau
 from .g8d_dampedoscillator import g8d_damped_oscillator
 from .g2d_hat import g2D_hat_function
 from .g2d_rp55 import g2D_rp55
+from .g21d_le_frame import g21D_rp201
 # from .g_fem_pushover import g_pushover
 
 REGISTRY = {}
@@ -12,4 +13,5 @@ REGISTRY["himmelblau"] = g2d_himmelblau
 REGISTRY["damped_oscillator"] = g8d_damped_oscillator
 REGISTRY["hat"] = g2D_hat_function
 REGISTRY["rp55"] = g2D_rp55
+REGISTRY["le_frame"] = g21D_rp201
 # REGISTRY["pushover_frame"] = g_pushover
