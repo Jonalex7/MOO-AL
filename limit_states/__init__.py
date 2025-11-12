9d58ba1d6b3055b135112d087dae6e8b7736d6bb
Jonathan <jmoran@uliege.be>
2025-11-12 14:57:25 +0100

    adding ls 2DOF oscillator

--- limit_states/__init__.py
+++ limit_states/__init__.py
@@ -4,6 +4,7 @@ from .g2d_hmcmc_himmelblau import g2d_himmelblau
 from .g2d_hat import g2D_hat_function
 from .g6d_nonlinear_oscillator import g6d_nonlinear_oscillator
 from .g_high_dimensional import gd_high_dimensional
+from .g8d_two_dof_oscillator import g8d_two_dof_oscillator
 
 REGISTRY = {}
 
@@ -13,3 +14,4 @@ REGISTRY["himmelblau"] = g2d_himmelblau
 REGISTRY["hat"] = g2D_hat_function
 REGISTRY["nonlinear_oscillator"] = g6d_nonlinear_oscillator
 REGISTRY["high_dimensional"] = gd_high_dimensional
+REGISTRY["2dof_oscillator"] = g8d_two_dof_oscillator
