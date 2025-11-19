9cdf72c986465e9e840524fd8401fcdfd2d82663
Jonathan <jmoran@uliege.be>
2025-11-19 17:09:48 +0100

    print LML track

--- main.py
+++ main.py
@@ -193,7 +193,7 @@ def main(config, name_exp):
         # b_stab = np.abs(B_model - b_j) / B_model   # relative difference with previous beta
         # b_j = B_model  # Update b_j for the next iteration
 
-        print(f'Pf_model: {Pf_model:.3E}, Pf_rel_diff: {Pf_rel_diff:.2%}, B_rel_diff: {B_rel_diff.item():.2%}, B_stab: {b_stab:.1%}')
+        print(f'Pf_model: {Pf_model:.3E}, Pf_rel_diff: {Pf_rel_diff:.2E}, B_rel_diff: {B_rel_diff.item():.2E}, LML = {lml:.2E}')
 
         # Making predictions of mean and std for mc population 
         x_mc_pool = np.random.normal(0, 1, size=(int(n_mcs_pool), lstate.input_dim))
