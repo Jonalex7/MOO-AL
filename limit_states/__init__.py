abade15de680287d9a9dde7d1092e86c416dde71
Jonathan Moran <jmoran@uliege.be>
2026-03-01 16:44:21 +0100

    adding three acquisition functions from wei_et_al

--- limit_states/__init__.py
+++ limit_states/__init__.py
@@ -5,6 +5,9 @@ from .g2d_hat import g2D_hat_function
 from .g6d_nonlinear_oscillator import g6d_nonlinear_oscillator
 from .g_high_dimensional import gd_high_dimensional
 from .g8d_two_dof_oscillator import g8d_two_dof_oscillator
+from .g1_wei import g2d_wei_g1
+from .g2_wei import g2d_wei_g2
+from .g3_wei import g3d_wei_g3
 
 REGISTRY = {}
 
@@ -15,3 +18,6 @@ REGISTRY["hat"] = g2D_hat_function
 REGISTRY["nonlinear_oscillator"] = g6d_nonlinear_oscillator
 REGISTRY["high_dimensional"] = gd_high_dimensional
 REGISTRY["2dof_oscillator"] = g8d_two_dof_oscillator
+REGISTRY["wei_g1"] = g2d_wei_g1
+REGISTRY["wei_g2"] = g2d_wei_g2
+REGISTRY["wei_g3"] = g3d_wei_g3
